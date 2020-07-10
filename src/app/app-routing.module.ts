695f184d1f4b615d988e660ccbf40167c2fb85a3
webmaster-pnn <machadopnn@gmail.com>
2020-07-10 14:38:44 -0300

    criacao da pagina home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
