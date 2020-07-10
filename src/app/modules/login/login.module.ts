29abbbdd5458ccc8c9611a6de1f468536d86ae0a
webmaster-pnn <machadopnn@gmail.com>
2020-07-10 16:05:35 -0300

    adicionando material design

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -5,13 +5,32 @@ import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+// material design
+import {MatFormFieldModule, MatInputModule } from '@angular/material';
+
+
 
 
 @NgModule({
   declarations: [LoginComponent, ForgotPasswordComponent],
   imports: [
+    // common
+    
     CommonModule,
-    LoginRoutingModule
+    // modules
+    LoginRoutingModule,
+    // angular forms
+    ReactiveFormsModule,
+    FormsModule,
+    // material design
+    MatFormFieldModule,
+    MatInputModule, 
+
+   
+    
   ]
 })
 export class LoginModule { }
