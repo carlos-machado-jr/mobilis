42441ba8c6776d492a4cff2dfe548d297a58b5f4
webmaster-pnn <machadopnn@gmail.com>
2020-07-12 09:48:36 -0300

    Criacao do modulo do novo componente reusavel

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,39 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { MatTableModule } from '@angular/material/table'
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule, MatPaginatorIntl } from '@angular/material/paginator';
-import { MAT_LABEL_GLOBAL_OPTIONS } from '@angular/material/core';
-import { TableComponent } from '../../shared/components/table/table.component'
+import { TableModule } from '../../shared/components/table/table.module'
 
 @NgModule({
-  declarations: [HomeComponent, TableComponent],
+  declarations: [HomeComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
-    ReactiveFormsModule,
-    FormsModule,
-    // material design
-    MatFormFieldModule,
-    MatInputModule, 
-    MatSortModule,
-    MatPaginatorModule,
-    MatIconModule,
-    MatButtonModule,
-    MatTableModule
+    TableModule,
+    
   ],
-  providers: [
-    {provide: 
-      MAT_LABEL_GLOBAL_OPTIONS, 
-      useValue: {float: 'auto'}
-      
-    },
-    { provide: MatPaginatorIntl, useClass: TableComponent}]
+  providers: []
+   
 })
 export class HomeModule { }
---
+++ src/app/shared/components/table/table.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TableComponent } from './table.component'
+import { MatPaginatorIntl, MatPaginatorModule } from '@angular/material/paginator';
+import { HomeRoutingModule } from 'src/app/modules/home/home-routing.module';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+
+
+
+@NgModule({
+  declarations: [TableComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    // material design
+    MatFormFieldModule,
+    MatInputModule, 
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule
+  ],
+  providers:[ { provide: MatPaginatorIntl, useClass: TableComponent}]
+})
+export class TableModule { }
