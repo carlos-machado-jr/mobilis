3d459b9168d3efaf1e597b23f7ef9d58f7a27b32
webmaster-pnn <machadopnn@gmail.com>
2020-07-16 01:43:56 -0300

    iniciando implementacao do cadastro de usuarios

--- src/app/modules/usuarios/usuarios-routing.module.ts
+++ src/app/modules/usuarios/usuarios-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsuariosComponent } from './pages/lista_completa/usuarios.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
-const routes: Routes = [{ path: '', component: UsuariosComponent }];
+const routes: Routes = [
+  { path: '', component: UsuariosComponent },
+  { path: 'adicionar', component: CadastroComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/usuarios/usuarios.module.ts
+++ src/app/modules/usuarios/usuarios.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { UsuariosRoutingModule } from './usuarios-routing.module';
 import { UsuariosComponent } from './pages/lista_completa/usuarios.component';
 import { TableModule } from 'src/app/shared/components/table/table.module';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 
 
 @NgModule({
-  declarations: [UsuariosComponent],
+  declarations: [UsuariosComponent, CadastroComponent],
   imports: [
     CommonModule,
     TableModule,
